22c0a359a89766008056749ea4667df6bee44d3a
Jānis Feldmanis <feldmanis45@gmail.com>
2025-10-25 18:19:58 +0300

    .env example file and added return type

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use App\Models\Task;
 
 class TaskController extends Controller
 {
 
-    public function index(Request $request)
+    public function index(Request $request): View
     {
         $status = $request->query('status');
 
@@ -25,7 +27,7 @@ class TaskController extends Controller
         return view('tasks.index', compact('tasks', 'status'));
     }
 
-    public function update(Request $request, Task $task)
+    public function update(Request $request, Task $task): RedirectResponse
     {
         $validated = $request->validate([
             'title' => 'required|string|max:50',
@@ -37,7 +39,7 @@ class TaskController extends Controller
         return redirect()->route('tasks.index')->with('success', 'Task updated successfully!');
     }
 
-    public function create(Request $request)
+    public function create(Request $request): RedirectResponse
     {
         $validated = $request->validate([
             'title' => 'required|string|max:255',
@@ -56,7 +58,7 @@ class TaskController extends Controller
         return redirect()->route('tasks.index')->with('success', 'Task deleted successfully.');
     }
 
-    public function toggleStatus(Task $task)
+    public function toggleStatus(Task $task): RedirectResponse
     {
         $task->is_completed = !$task->is_completed;
         $task->save();
